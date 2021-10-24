c78af56be44002acf06eb5713dabba23593fd786
tekkenkim <gmrrltkxogus@naver.com>
2021-10-24 13:58:32 +0900

    to reload add something

--- src/db.js
+++ src/db.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
