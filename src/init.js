90023f6b00905ffdc37e192d42f3741b18092eb8
tekkenkim <gmrrltkxogus@naver.com>
2021-10-24 14:06:56 +0900

    port env var

--- src/init.js
+++ src/init.js
@@ -5,7 +5,7 @@ import "./models/Video.js";
 import "./models/User.js";
 import "./models/Comment";
 import app from "./server.js";
-const PORT = 8080;
+const PORT = process.env.PORT || 4000;
 const handleListening = () => {
   console.log(`✅ Server is listening on ${PORT} port 🚀`);
 };
